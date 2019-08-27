ef82a78d6f9346f5216dcacd98596af837438410
sgswtky <sugisawa_takaya@cyberagent.co.jp>
2019-08-27 13:58:04 +0900

    chapter-9

--- src/test/java/money/MoneyTest.java
+++ src/test/java/money/MoneyTest.java
@@ -1,6 +1,7 @@
 package money;
 
 import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 public class MoneyTest {
@@ -10,6 +11,7 @@ public class MoneyTest {
         assertEquals(Money.dollar(10), five.times(2));
         assertEquals(Money.dollar(15), five.times(3));
     }
+
     @Test
     public void testEquality() {
         assertTrue(Money.dollar(5).equals(Money.dollar(5)));
@@ -18,10 +20,17 @@ public class MoneyTest {
         assertFalse(Money.franc(5).equals(Money.franc(6)));
         assertFalse(Money.franc(5).equals(Money.dollar(5)));
     }
+
     @Test
     public void testFrancMultiplication() {
         Money five = Money.franc(5);
         assertEquals(Money.franc(10), five.times(2));
         assertEquals(Money.franc(15), five.times(3));
+    }
+
+    @Test
+    public void testCurrency() {
+        assertEquals("USD", Money.dollar(1).currency());
+        assertEquals("CHF", Money.franc(1).currency());
     }
 }
